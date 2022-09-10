f74dc0dc5c5c9bf206b6ad45d799c8e3890cf71b
Abdullahi <oyelakinoyedele@gmail.com>
2022-09-09 19:52:09 -0700

    prints base 16 numbers

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,12 +9,18 @@
 int main(void)
 {
 	int num = 0;
+	char alpha = 'a';
 
 	while (num < 10)
 	{
-		putchar(num);
-		putchar("%o");
+		putchar((num % 10) + '0');
 		num++;
+	}
+
+	while (alpha <= 'f')
+	{
+		putchar(alpha);
+		alpha++;
 	}
 
 	putchar('\n');
