e2fb7110ba51a764a0e25d05ce322480f992d38b
Abdullahi <oyelakinoyedele@gmail.com>
2022-09-14 10:06:00 -0700

    read english alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
+#include "main.h"
+
 /**
  * main - printd alphabet in lowercase
  *
  * Return: Always 0 (Success)
  */
 
-#include "main.h"
-
 void print_alphabet(void)
 {
 	char alpha;
-	
+
 	for (alpha = 97; alpha < 123; alpha++)
 	{
 		_putchar(alpha);
