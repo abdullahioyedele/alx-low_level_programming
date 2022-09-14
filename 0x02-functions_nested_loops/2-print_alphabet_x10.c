e82f4b9c28bf4f784c91540cc0f276e10c08c524
Abdullahi <oyelakinoyedele@gmail.com>
2022-09-14 10:27:32 -0700

    repeat alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10 - prints english alphabets in lowercase 10 times
+ */
+
 void print_alphabet_x10(void)
 {
 	char letters;
@@ -8,5 +12,9 @@ void print_alphabet_x10(void)
 	for (times = 0; times < 10; times++)
 	{
 		for (letters = 97; letters < 123; letters++)
+		{
 			_putchar(letters);
+		}
+		_putchar('\n');
 	}
+}
