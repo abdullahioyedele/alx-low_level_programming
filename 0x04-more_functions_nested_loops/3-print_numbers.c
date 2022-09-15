df2d8f16931e132ad8bff094bf6e54ad6ac187d3
Abdullahi <oyelakinoyedele@gmail.com>
2022-09-15 14:03:47 -0700

    prints 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 - 9
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		_putchar(num);
+	}
+
+	_putchar ('\n');
+}
