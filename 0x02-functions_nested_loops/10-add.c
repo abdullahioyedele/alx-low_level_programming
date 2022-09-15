9e2badb44e3321eeb578b31e2d47e552f5ab55c3
Abdullahi <oyelakinoyedele@gmail.com>
2022-09-14 21:11:01 -0700

    sum up two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - sum up two integers
+ *
+ * @num1: First integer to be added
+ *
+ * @num2: second integer to be added
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2)
+}
